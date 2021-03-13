c2c485ff5098d0de3874e76d00cf96bae70b93e0
Attila Soltész <60141838+solattila@users.noreply.github.com>
2021-03-13 20:13:15 +0100

    add constants and naming convention

---
+++ fundamentals/constants_and_naming_conventions.cpp
@@ -0,0 +1,23 @@
+/*
+    Created by: Attila Soltész
+    
+*/
+
+#include<iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    const double MY_PI = 3.14159; // const naming convention 
+
+    // MY_PI = 3.5634; not work variable is constant
+
+    int myTaxRate = 4; //camelcasing variable naming convention
+
+    const string MY_NAME = "Attila";
+
+    cout << MY_NAME << endl;
+
+    return 0;
+}
